a8a4dad477a4a4b9e16e9a421cb2a16df450b50a
Caedmon Myers <caedmonmyers@icloud.com>
2025-02-01 14:47:53 -0600

    Changes to auto stuff

--- src/main/java/frc/robot/Aquamarine.java
+++ src/main/java/frc/robot/Aquamarine.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 //import com.pathplanner.lib.PathPlanner;
@@ -15,59 +16,67 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 public class Aquamarine {
     
     public static Command driveByTime(CommandSwerveDrivetrain drivetrain, com.ctre.phoenix6.swerve.SwerveRequest.FieldCentric drive){
-        
-        return Commands.sequence(Commands.print("driveByTime started"),
-            Commands.print("applyRequest starting"),
+        return Commands.sequence(
+            Commands.print("Starting wait command"),
             drivetrain.applyRequest(
                 () -> {
-                    System.out.print("drive.withVelocityX starting");
-                    return (SwerveRequest) drive.withVelocityX(0.0);
-                }
-            ).withTimeout(5),
-            drivetrain.runOnce(
-                () -> {
-                    drive.withVelocityX(0.0);
+                    return (SwerveRequest) drive.withVelocityX(0.0).withVelocityY(0.0);
+                    
                 }
             ),
-            Commands.print("wait starting"),
-            Commands.waitSeconds(5),
-            Commands.print("wait ended"),
-            drivetrain.applyRequest(
-                () -> {
-                    System.out.print("-drive.withVelocityY starting");
-                    return (SwerveRequest) drive.withVelocityY(0.0);
-                }
-            ).withTimeout(5),
-            drivetrain.runOnce(
-                () -> {
-                    drive.withVelocityY(0.0);
-                }
-            ),
-            Commands.waitSeconds(5),
-             drivetrain.applyRequest(() -> {
-                    System.out.print("-drive.withVelocityX starting");
-                    return (SwerveRequest) drive.withVelocityX(0.0);
-                }
-            ).withTimeout(5),
-            drivetrain.runOnce(
-                () -> {
-                    drive.withVelocityX(0.0);
-                }
-            ),
-            Commands.waitSeconds(5),
-            drivetrain.run(
-                () -> {
-                    System.out.print("drive.withVelocityY starting");
-                    drive.withVelocityY(0.0);
-                }
-            ).withTimeout(5),
-            drivetrain.runOnce(
-                () -> {
-                    System.out.println("drive back to 0.0");
-                    drive.withVelocityY(0.0);
-                }
-            ),
-            Commands.print("withVelocity back to 0.0")
+            Commands.waitSeconds(5)
+            // Commands.print("driveByTime started"),
+            // Commands.print("applyRequest starting"),
+            // drivetrain.applyRequest(
+            //     () -> {
+            //         System.out.print("drive.withVelocityX starting");
+            //         return (SwerveRequest) drive.withVelocityX(0.0);
+            //     }
+            // ).withTimeout(5),
+            // drivetrain.runOnce(
+            //     () -> {
+            //         drive.withVelocityX(0.0);
+            //     }
+            // ),
+            // Commands.print("wait starting"),
+            // Commands.waitSeconds(5),
+            // Commands.print("wait ended"),
+            // drivetrain.applyRequest(
+            //     () -> {
+            //         System.out.print("-drive.withVelocityY starting");
+            //         return (SwerveRequest) drive.withVelocityY(0.0);
+            //     }
+            // ).withTimeout(5),
+            // drivetrain.runOnce(
+            //     () -> {
+            //         drive.withVelocityY(0.0);
+            //     }
+            // ),
+            // Commands.waitSeconds(5),
+            //  drivetrain.applyRequest(() -> {
+            //         System.out.print("-drive.withVelocityX starting");
+            //         return (SwerveRequest) drive.withVelocityX(0.0);
+            //     }
+            // ).withTimeout(5),
+            // drivetrain.runOnce(
+            //     () -> {
+            //         drive.withVelocityX(0.0);
+            //     }
+            // ),
+            // Commands.waitSeconds(5),
+            // drivetrain.run(
+            //     () -> {
+            //         System.out.print("drive.withVelocityY starting");
+            //         drive.withVelocityY(0.0);
+            //     }
+            // ).withTimeout(5),
+            // drivetrain.runOnce(
+            //     () -> {
+            //         System.out.println("drive back to 0.0");
+            //         drive.withVelocityY(0.0);
+            //     }
+            // ),
+            // Commands.print("withVelocity back to 0.0")
         );
     }
 

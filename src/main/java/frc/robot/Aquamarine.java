5e78f98bd8d1d479e5cabb9e7a176f796ed4e940
Caedmon Myers <caedmonmyers@icloud.com>
2025-02-01 15:07:59 -0600

    test

--- src/main/java/frc/robot/Aquamarine.java
+++ src/main/java/frc/robot/Aquamarine.java
@@ -25,58 +25,6 @@ public class Aquamarine {
                 }
             ),
             Commands.waitSeconds(5)
-            // Commands.print("driveByTime started"),
-            // Commands.print("applyRequest starting"),
-            // drivetrain.applyRequest(
-            //     () -> {
-            //         System.out.print("drive.withVelocityX starting");
-            //         return (SwerveRequest) drive.withVelocityX(0.0);
-            //     }
-            // ).withTimeout(5),
-            // drivetrain.runOnce(
-            //     () -> {
-            //         drive.withVelocityX(0.0);
-            //     }
-            // ),
-            // Commands.print("wait starting"),
-            // Commands.waitSeconds(5),
-            // Commands.print("wait ended"),
-            // drivetrain.applyRequest(
-            //     () -> {
-            //         System.out.print("-drive.withVelocityY starting");
-            //         return (SwerveRequest) drive.withVelocityY(0.0);
-            //     }
-            // ).withTimeout(5),
-            // drivetrain.runOnce(
-            //     () -> {
-            //         drive.withVelocityY(0.0);
-            //     }
-            // ),
-            // Commands.waitSeconds(5),
-            //  drivetrain.applyRequest(() -> {
-            //         System.out.print("-drive.withVelocityX starting");
-            //         return (SwerveRequest) drive.withVelocityX(0.0);
-            //     }
-            // ).withTimeout(5),
-            // drivetrain.runOnce(
-            //     () -> {
-            //         drive.withVelocityX(0.0);
-            //     }
-            // ),
-            // Commands.waitSeconds(5),
-            // drivetrain.run(
-            //     () -> {
-            //         System.out.print("drive.withVelocityY starting");
-            //         drive.withVelocityY(0.0);
-            //     }
-            // ).withTimeout(5),
-            // drivetrain.runOnce(
-            //     () -> {
-            //         System.out.println("drive back to 0.0");
-            //         drive.withVelocityY(0.0);
-            //     }
-            // ),
-            // Commands.print("withVelocity back to 0.0")
         );
     }
 

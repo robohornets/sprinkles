de0099f032f9a5d02a5ae6bc80e68344af439695
Glorp450 <jonasb0707@student.k12.tulsaschools.org>
2025-02-15 15:16:02 -0600

    added coral stuff

--- src/main/java/frc/robot/subsystems/mechanisms/coral/CoralVariables.java
+++ src/main/java/frc/robot/subsystems/mechanisms/coral/CoralVariables.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems.mechanisms.coral;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Encoder;
+
 public class CoralVariables {
+    public static TalonFX angleMotor = new TalonFX(12);
+    public static TalonFX flywheelMotor = new TalonFX(11);
+    public static final DutyCycleEncoder angleAbsoluteEncoder = new DutyCycleEncoder(2);
+
+    public static Boolean angleDisabled = false;
+    public static Boolean flywheelDisabled = false;
     
+    // Angle 12, flywheel 11
+    public static Double angleSpeed = 0.2;
+    public static Double flywheelSpeed = 0.2;
+
 }
